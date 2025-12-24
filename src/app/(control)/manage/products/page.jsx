4d718e3ce02f7a7d89d05d26cd7a8711fd6774c5
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-25 03:15:35 +0600

    update

--- src/app/(control)/manage/products/page.jsx
+++ src/app/(control)/manage/products/page.jsx
@@ -1,9 +1,14 @@
+import AddProduct from '@/components/forms/AddProduct'
 import React from 'react'
 
 const Products = () => {
   return (
-    <div>
-      Products
+    <div className='w-full p-4 flex flex-col items-center gap-6'>
+      <h1 className='text-2xl font-semibold w-full text-center border-b-2 border-black/10'>Products</h1>
+        <AddProduct/>
+      <div className='w-full flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-xl font-semibold text-center'>Project List</h1>
+      </div>
     </div>
   )
 }
---
+++ src/components/forms/AddProduct.jsx
@@ -0,0 +1,112 @@
+'use client'
+import axios from 'axios'
+import React, { useState } from 'react'
+
+
+const categories = [
+    {
+        id: 1,
+        category: 'Meals',
+        value: 'meals'
+    },
+    {
+        id: 2,
+        category: 'Combo',
+        value: 'combo'
+    },
+    {
+        id: 3,
+        category: 'Snacks',
+        value: 'snacks'
+    },
+    {
+        id: 4,
+        category: 'Salad',
+        value: 'salad'
+    },
+    {
+        id: 5,
+        category: 'Drinks',
+        value: 'drinks'
+    },
+    {
+        id: 6,
+        category: 'Dessert',
+        value: 'dessert'
+    }
+]
+
+
+const AddProduct = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        price: '',
+        category: '',
+        image: null,
+
+    })
+
+    const handleChange = (e) => {
+        const { name, value, files } = e.target
+        if (files) {
+            setFormData({ ...formData, image: files[0] })
+        } else {
+            setFormData({ ...formData, [name]: value })
+        }
+    }
+
+    const addNewProduct = async (e) => {
+        e.preventDefault()
+        try {
+            const newData = await FormData()
+            newData.append('title', formData.title)
+            newData.append('description', formData.description)
+            newData.append('price', formData.price)
+            newData.append('category', formData.category)
+            newData.append('image', formData.image)
+
+            const response = await axios.post('/api/product', newData, { withCredentials: true })
+            alert(response.data.message)
+
+        } catch (error) {
+            console.log(error)
+            alert(error?.response?.data?.message || 'Failed to add product')
+        }
+
+    }
+    return (
+        <form onSubmit={addNewProduct} className='w-full flex flex-col items-center justify-center gap-4 border-b-2 border-black/10 p-4'>
+            <h1 className='text-xl font-semibold'>Add New Product</h1>
+            <div className='w-full flex flex-col gap-2'>
+                <label htmlFor="title">Title</label>
+                <input type="text" name='title' id='title' required value={formData.title} onChange={handleChange} className='w-full p-1 px-3 outline-none border-2 border-black/10 rounded-lg shadow-sm' />
+            </div>
+            <div className='w-full flex flex-col gap-2'>
+                <label htmlFor="description">Description</label>
+                <input type="text" name='description' id='description' required value={formData.description} onChange={handleChange} className='w-full p-1 px-3 outline-none border-2 border-black/10 rounded-lg shadow-sm' />
+            </div>
+            <div className='w-full flex flex-col gap-2'>
+                <label htmlFor="category">Category</label>
+                <select name="category" id="category" required value={formData.category} onChange={handleChange} className='w-full p-1 px-3 outline-none border-2 border-black/10 rounded-lg shadow-sm'>
+                    <option value="">--Select--</option>
+                    {categories && categories.map((cat) => (
+                        <option value={cat.value} key={cat.id}>{cat.value}</option>
+                    ))}
+                </select>
+            </div>
+            <div className='w-full flex flex-col gap-2'>
+                <label htmlFor="price">Price</label>
+                <input type="number" name='price' id='price' min={0} required value={formData.price} onChange={handleChange} className='w-full p-1 px-3 outline-none border-2 border-black/10 rounded-lg shadow-sm' />
+            </div>
+            <div className='w-full flex flex-col gap-2'>
+                <label htmlFor="image">Image</label>
+                <input type="file" accept='image/*' required name='image' value={FormData.image} id='image' className='w-full p-1 px-3 outline-none border-2 border-black/10 rounded-lg shadow-sm' />
+            </div>
+            <button type='submit' className='bg-black text-white p-1 px-4 rounded-lg shadow-sm cursor-pointer'>Submit</button>
+
+        </form>
+    )
+}
+
+export default AddProduct
