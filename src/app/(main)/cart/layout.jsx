9837e9eb14a8607b1602abed0fc7adc4d1d45615
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-26 05:12:48 +0600

    cart

---
+++ src/app/(main)/cart/layout.jsx
@@ -0,0 +1,23 @@
+import { isLogin } from '@/lib/middleware'
+import { redirect } from 'next/navigation'
+import React from 'react'
+export const metadata={
+    title:'Cart | Restaurant',
+    description:'Cart page'
+}
+
+const CartLayout = async({children}) => {
+    const auth= await isLogin()
+
+    if(!auth.success){
+        return redirect('/login')
+    }
+
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default CartLayout
