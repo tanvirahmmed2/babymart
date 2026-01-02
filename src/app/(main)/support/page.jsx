f9461e83c3e3137d18b5daade0a44ea893e9f3c8
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-02 12:07:31 +0600

    ui

--- src/app/(main)/support/page.jsx
+++ src/app/(main)/support/page.jsx
@@ -33,7 +33,7 @@ const Support = () => {
     <div className='w-full min-h-screen p-4 flex flex-col md:flex-row items-center justify-center gap-6'>
       <div className='w-full h-full flex flex-col gap-4'>
         <h1 className='text-2xl font-semibold text-center'>Join Us at Our Table</h1>
-        <p className='text-wrap w-full'>Whether you are planning a romantic evening, a family celebration, or a private event, we are here to make it unforgettable.</p>
+        <p className='text-wrap w-full text-center'>Whether you are planning a romantic evening, a family celebration, or a private event, we are here to make it unforgettable.</p>
 
       </div>
       <div className='w-full flex flex-col items-center justify-center gap-4'>
