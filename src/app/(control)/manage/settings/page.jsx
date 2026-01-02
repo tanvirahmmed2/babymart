fad658e6dbc67c45236388200a5c30b0e627f135
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-03 02:22:33 +0600

    api

--- src/app/(control)/manage/settings/page.jsx
+++ src/app/(control)/manage/settings/page.jsx
@@ -35,6 +35,78 @@ const Setting = () => {
       toast.error(error?.response?.data?.message || 'Failed to perform action')
     }
   }
+  const changeAddress = async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('/api/website/address', {id:formData.id, address:formData.address}, {withCredentials:true})
+      toast.success(response.data.message)
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to perform action')
+    }
+  }
+  const changeTax = async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('/api/website/tax', {id:formData.id, tax:formData.tax}, {withCredentials:true})
+      toast.success(response.data.message)
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to perform action')
+    }
+  }
+
+  const changeBio = async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('/api/website/bio', {id:formData.id, bio:formData.bio}, {withCredentials:true})
+      toast.success(response.data.message)
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to perform action')
+    }
+  }
+  const changeSocialLink = async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('/api/website/sociallink', {id:formData.id, socialLink:formData.socialLink}, {withCredentials:true})
+      toast.success(response.data.message)
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to perform action')
+    }
+  }
+  const changeOpenFrom = async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('/api/website/openfrom', {id:formData.id, openFrom:formData.openFrom}, {withCredentials:true})
+      toast.success(response.data.message)
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to perform action')
+    }
+  }
+  const changeOpenTo = async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('/api/website/opento', {id:formData.id, openTo:formData.openTo}, {withCredentials:true})
+      toast.success(response.data.message)
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to perform action')
+    }
+  }
+
+  const changeTagline = async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('/api/website/tagline', {id:formData.id, tagline:formData.tagline}, {withCredentials:true})
+      toast.success(response.data.message)
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to perform action')
+    }
+  }
 
   if (siteData === null) return <WebsiteDetails />
 
@@ -50,49 +122,49 @@ const Setting = () => {
         </div>
       </form>
 
-      <form onSubmit={changeTitle} className='w-full flex flex-col gap-2'>
+      <form onSubmit={changeBio} className='w-full flex flex-col gap-2'>
         <label htmlFor="bio">Bio</label>
         <div className='w-full flex flex-row items-center justify-between gap-4'>
           <input type="text" required value={formData.bio} id='bio' name='bio' onChange={handleChange} className='w-full p-1 px-3 border outline-none rounded-lg' />
           <button type='submit' className='px-4 p-1 bg-black text-center rounded-lg hover:scale-[1.02] transform ease-in-out duration-500 cursor-pointer text-white'>Change</button>
         </div>
       </form>
-      <form onSubmit={changeTitle} className='w-full flex flex-col gap-2'>
+      <form onSubmit={changeTagline} className='w-full flex flex-col gap-2'>
         <label htmlFor="tagline">Tagline</label>
         <div className='w-full flex flex-row items-center justify-between gap-4'>
           <input type="text" required value={formData.tagline} id='tagline' name='tagline' onChange={handleChange} className='w-full p-1 px-3 border outline-none rounded-lg' />
           <button type='submit' className='px-4 p-1 bg-black text-center rounded-lg hover:scale-[1.02] transform ease-in-out duration-500 cursor-pointer text-white'>Change</button>
         </div>
       </form>
-      <form onSubmit={changeTitle} className='w-full flex flex-col gap-2'>
+      <form onSubmit={changeAddress} className='w-full flex flex-col gap-2'>
         <label htmlFor="address">Address</label>
         <div className='w-full flex flex-row items-center justify-between gap-4'>
           <input type="text" required value={formData.address} id='address' name='address' onChange={handleChange} className='w-full p-1 px-3 border outline-none rounded-lg' />
           <button type='submit' className='px-4 p-1 bg-black text-center rounded-lg hover:scale-[1.02] transform ease-in-out duration-500 cursor-pointer text-white'>Change</button>
         </div>
       </form>
-      <form onSubmit={changeTitle} className='w-full flex flex-col gap-2'>
+      <form onSubmit={changeTax} className='w-full flex flex-col gap-2'>
         <label htmlFor="tax">Tax</label>
         <div className='w-full flex flex-row items-center justify-between gap-4'>
           <input type="number" required value={formData.tax} id='tax' name='tax' onChange={handleChange} className='w-full p-1 px-3 border outline-none rounded-lg' />
           <button type='submit' className='px-4 p-1 bg-black text-center rounded-lg hover:scale-[1.02] transform ease-in-out duration-500 cursor-pointer text-white'>Change</button>
         </div>
       </form>
-      <form onSubmit={changeTitle} className='w-full flex flex-col gap-2'>
+      <form onSubmit={changeOpenFrom} className='w-full flex flex-col gap-2'>
         <label htmlFor="openFrom">Open Time</label>
         <div className='w-full flex flex-row items-center justify-between gap-4'>
           <input type="text" required value={formData.openFrom} id='openFrom' name='openFrom' onChange={handleChange} className='w-full p-1 px-3 border outline-none rounded-lg' />
           <button type='submit' className='px-4 p-1 bg-black text-center rounded-lg hover:scale-[1.02] transform ease-in-out duration-500 cursor-pointer text-white'>Change</button>
         </div>
       </form>
-      <form onSubmit={changeTitle} className='w-full flex flex-col gap-2'>
+      <form onSubmit={changeOpenTo} className='w-full flex flex-col gap-2'>
         <label htmlFor="openTo">Open Till</label>
         <div className='w-full flex flex-row items-center justify-between gap-4'>
           <input type="text" required value={formData.openTo} id='openTo' name='openTo' onChange={handleChange} className='w-full p-1 px-3 border outline-none rounded-lg' />
           <button type='submit' className='px-4 p-1 bg-black text-center rounded-lg hover:scale-[1.02] transform ease-in-out duration-500 cursor-pointer text-white'>Change</button>
         </div>
       </form>
-      <form onSubmit={changeTitle} className='w-full flex flex-col gap-2'>
+      <form onSubmit={changeSocialLink} className='w-full flex flex-col gap-2'>
         <label htmlFor="socialLink">Social Link</label>
         <div className='w-full flex flex-row items-center justify-between gap-4'>
           <input type="text" required value={formData.socialLink} id='socialLink' name='socialLink' onChange={handleChange} className='w-full p-1 px-3 border outline-none rounded-lg' />
