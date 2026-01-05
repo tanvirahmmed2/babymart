862d96f4377dc73b3a2c59df89602657ea0be36b
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-05 18:47:55 +0600

    filter added

---
+++ src/app/api/product/filter/route.js
@@ -0,0 +1,22 @@
+import ConnectDB from "@/lib/database/mongo";
+import Product from "@/lib/models/product";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+    try {
+        await ConnectDB();
+        const { searchParams } = new URL(req.url);
+        const category = searchParams.get('category');
+        const availability = searchParams.get('availability');
+
+        const filter = {};
+        if (category) filter.category = category;
+        if (availability === "isAvailable") filter.quantity = { $gt: 0 };
+
+        const products = await Product.find(filter).sort({ _id: -1 });
+
+        return NextResponse.json({ success: true,message:'Successfully fetched data', payload: products });
+    } catch (error) {
+        return NextResponse.json({ success: false, message: 'Failed to fetch data', error: error.message }, { status: 500 });
+    }
+}
